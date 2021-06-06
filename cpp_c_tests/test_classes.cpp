db1cbf8945703afae90315783c80173a411b58b7
Person20345 <45317009+Person20345@users.noreply.github.com>
2021-06-06 13:43:28 +0300

    tests

--- cpp_c_tests/test_classes.cpp
+++ cpp_c_tests/test_classes.cpp
@@ -16,7 +16,7 @@ public:
 
 	void set(int index, int value) {
 		if (index > size_ && index < 0) {
-			std::cerr << "Error\n";
+			std::cerr << "Error 'set'\n";
 			exit(1);
 		}
 
@@ -25,34 +25,42 @@ public:
 
 	int get(int index) const {
 		if (index > size_&& index < 0) {
-			std::cerr << "Error\n";
+			std::cerr << "Error 'get'\n";
 			exit(1);
 		}
 
 		return array_[index];
 	}
 
-	friend A operator + (A& a1, A& a2);
+	A operator + (const A& a) {
+		if (this->length() != a.length()) {
+			std::cerr << "Error '+'\n";
+			exit(1);
+		}
+
+		A ret(this->length());
+
+		for(int i = 0; i < this->length(); i++)
+			ret.set(i, this->get(i) + a.get(i));
+
+		return ret;
+	}
+
+	void operator = (const A& a) {
+		if (this->length() != a.length()) {
+			std::cerr << "Error '='\n";
+			exit(1);
+		}
+		
+		for (int i = 0; i < this->length(); i++)
+			this->set(i, a.get(i));
+	}
 
 	~A() {
 		std::cout << "Destructor" << "\n";
 		delete[] array_;
 	}
 };
-
-A operator + (A& a1, A& a2) {
-	if (a1.length() != a2.length()) {
-		std::cerr << "Error\n";
-		exit(1);
-	}
-	
-	A ret(a1.length());
-
-	for(int i = 0; i < a1.length(); i++)
-		ret.set(i, a1.get(i) + a2.get(i));
-
-	return ret;
-}
 
 
 
@@ -77,7 +85,7 @@ int main() {
 	print(&array1);
 	print(&array2);
 
-	array3 = array1 + array2;
+	array3 = array1 + array2 + array2;
 
 	print(&array3);
 
